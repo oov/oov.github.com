83dc07d8efea140d08a86dedd198c8d8db66425a
oov <ilovevipper@gmail.com>
2016-01-26 17:50:16 +0900

    update psdtool

--- psdtool/js/main.go
+++ psdtool/js/main.go
@@ -3,8 +3,7 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
+	"errors"
 	"image/color"
 	"io"
 	"log"
@@ -15,9 +14,11 @@ import (
 )
 
 type root struct {
-	Width  int
-	Height int
-	Layer  []layer
+	Width     int
+	Height    int
+	Layer     []layer
+	processed int
+	progress  func(l *layer)
 }
 
 type layer struct {
@@ -25,6 +26,7 @@ type layer struct {
 	BlendMode             string
 	Opacity               uint8
 	Clipping              bool
+	BlendClippedElements  bool
 	TransparencyProtected bool
 	Visible               bool
 	X                     int
@@ -34,27 +36,27 @@ type layer struct {
 	Folder                bool
 	FolderOpen            bool
 	Canvas                *js.Object
+	MaskCanvas            *js.Object
 	Layer                 []layer
 	psdLayer              *psd.Layer
 }
 
 func main() {
 	js.Global.Set("parsePSD", parsePSD)
-	// js.Global.Set("exportZIP", exportZIP)
 }
 
 func arrayBufferToByteSlice(a *js.Object) []byte {
 	return js.Global.Get("Uint8Array").New(a).Interface().([]byte)
 }
 
-func buildLayer(l *layer) error {
+func (r *root) buildLayer(l *layer) error {
 	var err error
 
 	l.Name = l.psdLayer.Name
 	l.BlendMode = l.psdLayer.BlendMode.String()
 	l.Opacity = l.psdLayer.Opacity
-	l.Clipping = l.psdLayer.Clipping != 0
-	l.TransparencyProtected = l.psdLayer.TransparencyProtected()
+	l.Clipping = l.psdLayer.Clipping
+	l.BlendClippedElements = l.psdLayer.BlendClippedElements
 	l.Visible = l.psdLayer.Visible()
 	l.X = l.psdLayer.Rect.Min.X
 	l.Y = l.psdLayer.Rect.Min.Y
@@ -63,14 +65,23 @@ func buildLayer(l *layer) error {
 	l.Folder = l.psdLayer.Folder()
 	l.FolderOpen = l.psdLayer.FolderIsOpen()
 
-	if l.psdLayer.HasImage() {
+	if l.psdLayer.HasImage() && l.psdLayer.Rect.Dx()*l.psdLayer.Rect.Dy() > 0 {
 		if l.Canvas, err = createCanvas(l.psdLayer); err != nil {
 			return err
 		}
 	}
+	if _, ok := l.psdLayer.Channel[-2]; ok && l.psdLayer.Mask.Rect.Dx()*l.psdLayer.Mask.Rect.Dy() > 0 {
+		if l.MaskCanvas, err = createMaskCanvas(l.psdLayer); err != nil {
+			return err
+		}
+	}
+
+	r.processed++
+	r.progress(l)
+
 	for i := range l.psdLayer.Layer {
 		l.Layer = append(l.Layer, layer{psdLayer: &l.psdLayer.Layer[i]})
-		if err = buildLayer(&l.Layer[i]); err != nil {
+		if err = r.buildLayer(&l.Layer[i]); err != nil {
 			return err
 		}
 	}
@@ -79,7 +90,7 @@ func buildLayer(l *layer) error {
 
 func createCanvas(l *psd.Layer) (*js.Object, error) {
 	if l.Picker.ColorModel() != color.NRGBAModel {
-		return nil, fmt.Errorf("Unsupported color mode")
+		return nil, errors.New("Unsupported color mode")
 	}
 
 	w, h := l.Rect.Dx(), l.Rect.Dy()
@@ -123,88 +134,109 @@ func createCanvas(l *psd.Layer) (*js.Object, error) {
 	return cvs, nil
 }
 
-func extractLayer(l *layer) {
-	if l.psdLayer.HasImage() {
-		js.Global.Get("document").Get("body").Call("appendChild", l.Canvas)
+func createMaskCanvas(l *psd.Layer) (*js.Object, error) {
+	m, ok := l.Channel[-2]
+	if !ok {
+		return nil, nil
 	}
-	for i := range l.Layer {
-		extractLayer(&l.Layer[i])
+
+	w, h := l.Mask.Rect.Dx(), l.Mask.Rect.Dy()
+	cvs := js.Global.Get("document").Call("createElement", "canvas")
+	cvs.Set("width", w)
+	cvs.Set("height", h)
+	ctx := cvs.Call("getContext", "2d")
+	imgData := ctx.Call("createImageData", w, h)
+	data := imgData.Get("data")
+
+	var ofsd, ofss, x, y, sx, dx int
+	mp := m.Data
+	for y = 0; y < h; y++ {
+		ofss = y * w
+		ofsd = ofss << 2
+		for x = 0; x < w; x++ {
+			sx, dx = ofss+x, ofsd+x<<2
+			data.SetIndex(dx+3, mp[sx])
+		}
 	}
+	ctx.Call("putImageData", imgData, 0, 0)
+	return cvs, nil
 }
 
-func parse(r io.Reader) (*root, error) {
+func countLayers(l []psd.Layer) int {
+	r := len(l)
+	for i := range l {
+		r += countLayers(l[i].Layer)
+	}
+	return r
+}
+
+type progressReader struct {
+	Buf      []byte
+	Progress func(float64)
+	pos      int
+}
+
+func (r *progressReader) Read(p []byte) (int, error) {
+	l := copy(p, r.Buf[r.pos:])
+	if l == 0 {
+		return 0, io.EOF
+	}
+	if r.pos & ^0x3ffff != (r.pos+l) & ^0x3ffff {
+		r.Progress(float64(r.pos+l) / float64(len(r.Buf)))
+	}
+	r.pos += l
+	return l, nil
+}
+
+func parse(b []byte, progress func(phase int, progress float64, l *layer)) (*root, error) {
 	s := time.Now().UnixNano()
-	psdImg, _, err := psd.Decode(r)
+	psdImg, _, err := psd.Decode(&progressReader{
+		Buf:      b,
+		Progress: func(p float64) { progress(0, p, nil) },
+	}, &psd.DecodeOptions{
+		SkipMergedImage: true,
+	})
 	if err != nil {
 		return nil, err
 	}
 	e := time.Now().UnixNano()
+	progress(0, 1, nil)
 	log.Println("Decode PSD Structure:", (e-s)/1e6)
 
 	if psdImg.Config.ColorMode != psd.ColorModeRGB {
-		return nil, fmt.Errorf("Unsupported color mode")
+		return nil, errors.New("Unsupported color mode")
 	}
 
 	s = time.Now().UnixNano()
-	var l root
-	l.Width = psdImg.Config.Rect.Dx()
-	l.Height = psdImg.Config.Rect.Dy()
+	numLayers := countLayers(psdImg.Layer)
+	r := &root{
+		Width:  psdImg.Config.Rect.Dx(),
+		Height: psdImg.Config.Rect.Dy(),
+	}
+	r.progress = func(l *layer) { progress(1, float64(r.processed)/float64(numLayers), l) }
 	for i := range psdImg.Layer {
-		l.Layer = append(l.Layer, layer{psdLayer: &psdImg.Layer[i]})
-		buildLayer(&l.Layer[i])
+		r.Layer = append(r.Layer, layer{psdLayer: &psdImg.Layer[i]})
+		r.buildLayer(&r.Layer[i])
 	}
 	e = time.Now().UnixNano()
 	log.Println("Build Canvas:", (e-s)/1e6)
-	return &l, nil
+	return r, nil
 }
 
-func parsePSD(in *js.Object) *root {
-	root, err := parse(bytes.NewBuffer(arrayBufferToByteSlice(in)))
-	if err != nil {
-		panic(err)
-	}
-	return root
+func parsePSD(in *js.Object, progress *js.Object, complete *js.Object, failed *js.Object) {
+	go func() {
+		next := time.Now()
+		root, err := parse(arrayBufferToByteSlice(in), func(phase int, prog float64, l *layer) {
+			if now := time.Now(); now.After(next) {
+				progress.Invoke(phase, prog, l)
+				time.Sleep(1) // anti-freeze
+				next = now.Add(100 * time.Millisecond)
+			}
+		})
+		if err != nil {
+			failed.Invoke(err.Error())
+			return
+		}
+		complete.Invoke(root)
+	}()
 }
-
-/*
-func buildZIP(w *zip.Writer, f string, l *layer) error {
-	for i, ll := range l.Layer {
-		buildZIP(w, fmt.Sprintf("%s_%02d-%s", f, i, ll.psdLayer.Name), &ll)
-	}
-	if !l.psdLayer.HasImage() {
-		return nil
-	}
-	imgBase64 := l.Canvas.Call("toDataURL", "image/png").Call("substring", 22).String()
-	b, err := base64.StdEncoding.DecodeString(imgBase64)
-	if err != nil {
-		return err
-	}
-	filename := fmt.Sprintf("%s.png", f)
-	o, err := w.CreateHeader(&zip.FileHeader{Name: filename, Method: zip.Store, Flags: 0x800})
-	if err != nil {
-		return err
-	}
-	_, err = o.Write(b)
-	return err
-}
-
-func exportZIP(in *js.Object) *js.Object {
-	root, err := parse(bytes.NewBuffer(arrayBufferToByteSlice(in)))
-	if err != nil {
-		panic(err)
-	}
-
-	buf := &bytes.Buffer{}
-	w := zip.NewWriter(buf)
-	s := time.Now().UnixNano()
-	for i, l := range root.Layer {
-		buildZIP(w, fmt.Sprintf("%02d-%s", i, l.psdLayer.Name), &l)
-	}
-	e := time.Now().UnixNano()
-	log.Println("Build ZIP:", (e-s)/1e6)
-	if err = w.Close(); err != nil {
-		panic(err)
-	}
-	return js.NewArrayBuffer(buf.Bytes())
-}
-*/
--- psdtool/js/main.js
+++ psdtool/js/main.js
@@ -22,33 +22,83 @@
          return false;
       }, false);
       dz.addEventListener('drop', function(e) {
+         this.classList.remove('psdtool-drop-active');
          if (e.dataTransfer.files.length > 0) {
-            loadAndExtract(e.dataTransfer.files[0]);
+            loadAndParse(e.dataTransfer.files[0]);
          }
          e.preventDefault();
          e.stopPropagation();
          return false;
       }, false);
       document.getElementById('inputfile').addEventListener('change', function(e) {
-         loadAndExtract(document.getElementById('inputfile').files[0]);
+         loadAndParse(document.getElementById('inputfile').files[0]);
       }, false);
       document.getElementById('samplefile').addEventListener('click', function(e) {
-         loadAndExtract('img/' + document.getElementById('samplefile').getAttribute('data-filename'));
+         loadAndParse('img/' + document.getElementById('samplefile').getAttribute('data-filename'));
       }, false);
    }
 
-   function loadAndExtract(file_or_url) {
-      document.body.innerHTML = 'Now Loading...';
-      loadAsArrayBuffer(file_or_url).then(function(ab) {
-         document.body.innerHTML = '';
-         try {
-            extract(ab);
-         } catch (e) {
-            document.body.innerHTML = e;
+   function loadAndParse(file_or_url) {
+      var fileOpenUi = document.getElementById('file-open-ui');
+      var manual = document.getElementById('manual');
+      var fileLoadingUi = document.getElementById('file-loading-ui');
+      var errorReportUi = document.getElementById('error-report-ui');
+      var bar = document.getElementById('progress-bar');
+
+      fileOpenUi.style.display = 'none';
+      manual.style.display = 'none';
+      fileLoadingUi.style.display = 'block';
+      errorReportUi.style.display = 'none';
+
+      var barCaptionContainer = bar.querySelector('.psdtool-progress-bar-caption');
+      var barCaption = document.createTextNode('0% Complete');
+      var captionContainer = document.getElementById('progress-caption');
+      var caption = document.createTextNode('Now loading...');
+      var errorMessageContainer = document.getElementById('error-message');
+      var errorMessage = document.createTextNode('');
+
+      barCaptionContainer.innerHTML = '';
+      barCaptionContainer.appendChild(barCaption);
+      captionContainer.innerHTML = '';
+      captionContainer.appendChild(caption);
+      errorMessageContainer.innerHTML = '';
+      errorMessageContainer.appendChild(errorMessage);
+
+      function progress(phase, progress, layer) {
+         var p, msg;
+         switch (phase) {
+            case 0:
+               p = (progress * 50).toFixed(0);
+               msg = 'Parsing psd file...';
+               break;
+            case 1:
+               p = (50 + progress * 50).toFixed(0)
+               msg = 'Drawing "' + layer.Name + '" layer image...';
+               break;
          }
-      }, function(e) {
-         document.body.innerHTML = 'cannot read the psd file';
-      });
+         bar.style.width = p + '%';
+         bar.setAttribute('aria-valuenow', p);
+         barCaption.textContent = p + '% Complete';
+         caption.textContent = p + '% ' + msg;
+      }
+
+      loadAsArrayBuffer(file_or_url)
+         .then(parse.bind(this, progress))
+         .then(initMain)
+         .then(function(main) {
+            fileLoadingUi.style.display = 'none';
+            fileOpenUi.style.display = 'none';
+            manual.style.display = 'none';
+            errorReportUi.style.display = 'none';
+            document.body.appendChild(main);
+         }, function(e) {
+            fileLoadingUi.style.display = 'none';
+            fileOpenUi.style.display = 'block';
+            manual.style.display = 'block';
+            errorReportUi.style.display = 'block';
+            errorMessage.textContent = e;
+            console.error(e);
+         });
    }
 
    function loadAsArrayBuffer(file_or_url) {
@@ -77,48 +127,123 @@
       return deferred.promise;
    }
 
-   function extract(arrayBuffer) {
-      var root = parsePSD(arrayBuffer);
+   function parse(progress, arrayBuffer) {
+      var deferred = m.deferred();
+      parsePSD(arrayBuffer, progress, function(root) {
+         deferred.resolve(root);
+      }, function(e) {
+         deferred.reject(e);
+      });
+      return deferred.promise;
+   }
 
-      var sideContainer = document.createElement('div');
-      sideContainer.id = 'side-container';
+   function initMain(root) {
+      var deferred = m.deferred();
+      setTimeout(function() {
+         try {
+            var sideContainer = document.createElement('div');
+            sideContainer.id = 'side-container';
 
-      var previewContainer = document.createElement('div');
-      previewContainer.id = 'preview-container';
+            var previewContainer = document.createElement('div');
+            previewContainer.id = 'preview-container';
 
-      var canvas = document.createElement('canvas');
-      canvas.width = root.Width;
-      canvas.height = root.Height;
+            var canvas = document.createElement('canvas');
+            canvas.width = root.Width;
+            canvas.height = root.Height;
 
-      sideContainer.appendChild(buildTree(root, render.bind(null, canvas, root)));
+            sideContainer.appendChild(buildTree(root, render.bind(null, canvas, root)));
 
-      var main = document.createElement('div');
-      main.id = 'main';
-      previewContainer.appendChild(canvas);
-      main.appendChild(sideContainer);
-      main.appendChild(previewContainer);
+            var main = document.createElement('div');
+            main.id = 'main';
+            previewContainer.appendChild(canvas);
+            main.appendChild(sideContainer);
+            main.appendChild(previewContainer);
 
-      render(canvas, root);
-      document.body.appendChild(main);
+            render(canvas, root);
+            deferred.resolve(main);
+         } catch (e) {
+            deferred.reject(e);
+         }
+      }, 1);
+      return deferred.promise;
+   }
+
+   function draw(ctx, src, x, y, opacity, blendMode) {
+      if (typeof opacity == 'object') {
+         switch (opacity.BlendMode) {
+            case 'normal':
+               blendMode = 'source-over';
+               break;
+            case 'darken':
+            case 'multiply':
+            case 'color-burn':
+
+            case 'lighten':
+            case 'screen':
+            case 'color-dodge':
+
+            case 'overlay':
+            case 'soft-light':
+            case 'hard-light':
+            case 'difference':
+            case 'exclusion':
+
+            case 'hue':
+            case 'saturation':
+            case 'color':
+            case 'luminosity':
+               blendMode = opacity.BlendMode;
+               break;
+         }
+         opacity = opacity.Opacity / 255;
+      }
+      ctx.globalAlpha = opacity;
+      ctx.globalCompositeOperation = blendMode;
+      ctx.drawImage(src, x, y);
    }
 
    function render(canvas, root) {
+
       function r(ctx, layer) {
          if (!layer.visibleInput.checked || layer.Opacity == 0) {
             return
          }
-         if (layer.Canvas) {
-            ctx.globalAlpha = layer.Opacity / 255;
-            switch (layer.BlendMode) {
-               case 'normal',
-               'darken', 'multiply', 'color-burn',
-               'lighten', 'screen', 'color-dodge',
-               'overlay', 'soft-light', 'hard-light',
-               'difference', 'exclusion',
-               'hue', 'saturation', 'color', 'luminosity':
-                  ctx.globalCompositeOperation = layer.BlendMode;
+         if (layer.Canvas && !layer.Clipping) {
+            if (layer.clip.length) {
+               if (layer.BlendClippedElements) {
+                  var bb = document.createElement('canvas');
+                  var bbctx = bb.getContext('2d');
+                  bb.width = layer.Width;
+                  bb.height = layer.Height;
+                  draw(bbctx, layer.Canvas, 0, 0, 1, 'source-over');
+                  for (var i = 0; i < layer.clip.length; ++i) {
+                     var child = layer.clip[i];
+                     if (!child.visibleInput.checked || child.Opacity == 0 || !child.Canvas) {
+                        continue;
+                     }
+                     draw(bbctx, child.Canvas, child.X - layer.X, child.Y - layer.Y, child);
+                  }
+                  draw(bbctx, layer.Canvas, 0, 0, 1, 'destination-in');
+                  draw(ctx, bb, layer.X, layer.Y, layer);
+               } else {
+                  draw(ctx, layer.Canvas, layer.X, layer.Y, layer);
+                  for (var i = 0; i < layer.clip.length; ++i) {
+                     var child = layer.clip[i];
+                     if (!child.visibleInput.checked || child.Opacity == 0 || !child.Canvas) {
+                        continue;
+                     }
+                     var bb = document.createElement('canvas');
+                     var bbctx = bb.getContext('2d');
+                     bb.width = child.Width;
+                     bb.height = child.Height;
+                     draw(bbctx, child.Canvas, 0, 0, 1, 'copy');
+                     draw(bbctx, layer.Canvas, layer.X - child.X, layer.Y - child.Y, 1, 'destination-in');
+                     draw(ctx, bb, child.X, child.Y, child);
+                  }
+               }
+            } else {
+               draw(ctx, layer.Canvas, layer.X, layer.Y, layer);
             }
-            ctx.drawImage(layer.Canvas, layer.X, layer.Y);
          }
          for (var i = 0; i < layer.Layer.length; ++i) {
             r(ctx, layer.Layer[i]);
@@ -144,8 +269,14 @@
          function r(layer) {
             if (layer.visibleInput.checked) {
                layer.li.classList.remove('psdtool-hidden');
+               for (var i = 0; i < layer.clip.length; ++i) {
+                  layer.clip[i].li.classList.remove('psdtool-hidden-by-clipping');
+               }
             } else {
                layer.li.classList.add('psdtool-hidden');
+               for (var i = 0; i < layer.clip.length; ++i) {
+                  layer.clip[i].li.classList.add('psdtool-hidden-by-clipping');
+               }
             }
             for (var i = 0; i < layer.Layer.length; ++i) {
                r(layer.Layer[i]);
@@ -156,9 +287,28 @@
          }
       }
 
+      function registerClippingGroup(layers) {
+         var clip = [];
+         for (var i = layers.length - 1; i >= 0; --i) {
+            var layer = layers[i];
+            registerClippingGroup(layer.Layer);
+            if (layer.Clipping) {
+               clip.unshift(layer);
+               layer.clip = [];
+            } else {
+               for (var j = 0; j < clip.length; ++j) {
+                  clip[j].clippedBy = layer;
+               }
+               layer.clip = clip;
+               clip = [];
+            }
+         }
+      }
+
       function r(ul, layer, parentLayer) {
          layer.id = ++cid;
          layer.parentLayer = parentLayer;
+
          var prop = buildLayerProp(layer, parentLayer);
          var children = document.createElement('ul');
          for (var i = layer.Layer.length - 1; i >= 0; --i) {
@@ -177,6 +327,9 @@
                for (var p = layer.parentLayer; p.visibleInput;) {
                   p.visibleInput.checked = true;
                   p = p.parentLayer;
+               }
+               if (layer.clippedBy) {
+                  layer.clippedBy.visibleInput.checked = true;
                }
                redraw();
                updateClass();
@@ -208,6 +361,8 @@
          r(ul, root.Layer[i], root);
       }
 
+      registerClippingGroup(root.Layer);
+
       var set = {};
       var radios = ul.querySelectorAll('.psdtool-layer-visible[type=radio]');
       for (var i = 0; i < radios.length; ++i) {
@@ -234,7 +389,6 @@
 
    function buildLayerProp(layer, parentLayer) {
       var name = document.createElement('label');
-      name.className = 'psdtool-layer-name';
       var visible = document.createElement('input');
       var layerName = layer.Name;
       switch (layerName.charAt(0)) {
@@ -271,6 +425,13 @@
          icon.setAttribute('aria-hidden', 'true');
          name.appendChild(icon);
       } else if (layer.Canvas) {
+         if (layer.Clipping) {
+            var clip = document.createElement('img');
+            clip.className = 'psdtool-clipped-mark';
+            clip.src = 'img/clipped.svg';
+            clip.alt = 'clipped mark';
+            name.appendChild(clip);
+         }
          var thumb = document.createElement('canvas');
          thumb.className = 'psdtool-thumbnail';
          thumb.width = 96;
@@ -292,6 +453,7 @@
       name.appendChild(document.createTextNode(layerName));
 
       var div = document.createElement('div');
+      div.className = 'psdtool-layer-name';
       div.appendChild(name);
       return div;
    }
